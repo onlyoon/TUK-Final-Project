35f23a3d9e3fd37a5ab56521536325f93ff8ea56
yoon <parkyoon97@gmail.com>
2023-03-30 15:03:36 +0000

    input CSS

--- src/components/Buyer/BuyerComponents/InfoForPay/DeliveryInfo.js
+++ src/components/Buyer/BuyerComponents/InfoForPay/DeliveryInfo.js
@@ -4,11 +4,27 @@ const DeliveryInfo = () => {
   return (
     <div className={classes.deliveryinfo}>
       <div className={classes.title}>배송지 정보</div>
-      <div>수령인</div>
-      <div>배송지명</div>
-      <div>연락처</div>
-      <div>이메일</div>
-      <div>배송지 주소</div>
+      <form>
+        <div>
+          수령인 : <input className={classes.d_receiver} />
+        </div>
+        <div>
+          배송지명 : <input className={classes.d_place} />
+        </div>
+        <div>
+          연락처 : <input className={classes.d_number} />
+        </div>
+        <div>
+          이메일 : <input className={classes.d_email} />
+        </div>
+        <div>
+          배송지 주소 : <input className={classes.d_address} />
+          <button>우편번호</button>
+        </div>
+        <div>
+          <input className={classes.d_address2} />
+        </div>
+      </form>
     </div>
   );
 };
