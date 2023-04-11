166e0c352292b881cb5e67023a1ee0fa2bed91b9
yoon <parkyoon97@gmail.com>
2023-04-11 14:37:48 +0000

    Update 13 files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BuyerWrap2 from "./components/Buyer/BuyerWrap2";
 import BuyerWrap1 from "./components/Buyer/BuyerWrap1";
 import EmptyPage from "./components/EmptyPage";
+import Modal from './components/Modal';
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<BuyerWrap2 />} />
         <Route path="/buyer" element={<BuyerWrap1 />} />
+        <Route path="/modal" element={<Modal />} />
         <Route path="*" element={<EmptyPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import classes from "./Modal.module.css";
+
+const Modal = () => {
+  return (
+    <div className={classes.modal}>
+      <div>모달창입니다.</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Modal;
